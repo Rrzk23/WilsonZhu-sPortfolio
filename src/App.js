1dedc8ff71f51907a4f72f9348c8f55894415206
Ke Zhu <z5423255@ad.unsw.edu.au>
2023-08-27 22:16:47 +1000

    connect index with app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import About from "./routes/About"
 import Portfolio from "./routes/Portfolio"
 import Contact  from "./routes/Contact";
 import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    < >
+    <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/About" element={<About/>}/>
         <Route path="/Portfolio" element={<Portfolio/>}/>
         <Route path="/Contact" element={<Contact/>}/>
       </Routes>
-    </>
+    </Router>
   );
 }
 
