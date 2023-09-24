b97d5e9abe6faa4cd352689ea66b3f2f191b67f9
Ke Zhu <z5423255@ad.unsw.edu.au>
2023-09-25 00:59:56 +1000

    adding statement

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Components/Navbar';
 import "./Home.css";
 import CV from "../assests/CV.pdf"
+import Statement from "../assests/CV.pdf"
 const Home = () => {
     return (
         <div id="header">
@@ -11,6 +12,7 @@ const Home = () => {
             <p>Full Stack Developer</p>
             <h1>Hi, I'm <span>Wilson</span> From Sydney!</h1>
             <a className='CV' href={CV}>My CV</a>
+            <a className='CV' id = "second" href={Statement}>Academic Statement</a>
           </div>
 
         </div>
