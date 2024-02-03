aac7c7b54b14fa96de5871dd076a63ccfeffaada
wilson <wilsonzhu2003@gmail.com>
2024-02-03 23:43:40 +1100

    completeing the description of the portfolio and add the scrolling

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -13,26 +13,26 @@ const Portfolio = () => {
     {
       image: wiseStay, 
       link: 'https://www.wisestay.com.au/',
-      description: 'This is a simplify version of Kafka ',
+      description: 'WiseStay is the first company I worked in Australia for web development. The website is designed by sitemodify and I coded the Javascript relate part such as flitering and connecting to database. Click to see the pages ',
       title:  'WiseStay website design and functionality',
     },
     {
       image: roboticsS, 
       link: robotics,
-      description: 'Project 2 Description',
+      description: 'This lab project requires me to code as a team to so that robot can explore, record, spot the markers in the maze. It requires understanding of different sensors and connect to different package in ROS2. We got 90/100 for the final outcome, click to watch the video and my contribution paper',
       title:  'Maze Exploration TurtleBot',
     },
     {
       image: eventStreaming,
       link: 'https://nw-syd-gitlab.cseunsw.tech/COMP2511/23T2/students/z5423255/assignment-iii',
-      description: 'Project 2 Description',
-      title:  'EventStreaming Java software design and engineering',
+      description: 'Java project that start from scratch. It is an event streaming java project inspired by Kafka, this project incurs the use of Git, testing cases and code design. Click to see the repo of the project and specification in README and also the test outcome',
+      title:  'Event Streaming Java software design and engineering',
     },
     {
       image: Satellite, 
       link: 'https://nw-syd-gitlab.cseunsw.tech/COMP2511/23T2/students/z5423255/assignment-i',
-      description: 'Satellite communication assignment in Java',
-      title:  '',
+      description: 'Satellite communication assignment in Java. Click to see the repo of the project and specification in README and also the test outcome',
+      title:  'Satellite',
     },
     // Add more projects as needed
   ];
